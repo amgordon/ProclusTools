3946f2dc69840de8b4ac775861c07eaa7d483acf
Alan Gordon <alan.m.gordon@gmail.com>
2013-07-01 13:27:45 -0700

    parallelize by run as well as subject

--- pm/exMat.m
+++ pm/exMat.m
@@ -1,5 +1,5 @@
 function [] = exMat(sa,part,pTot)
 addpath(genpath('/biac4/wagner/biac3/wagner5/alan/perceptMnemonic/scripts/analysis'));
 addpath(genpath('/biac4/wagner/biac3/wagner5/alan/toolboxes'))
-PM_run_mvpa_general(sa, 'mnem', ['trainLocTestPerc_noFS' prepend(sa)])
-%PM_run_mvpa_general(num2str(sa), 'perc', ['trainPercLoc_testPerc_allOTInteractions' prepend(sa)], [1, 10])
+PM_run_mvpa_general(sa, 'mnem', ['SPLSigIntensity' prepend(sa)])
+%PM_run_mvpa_general(sa, 'perc', ['trainPercLoc_testPerc_allOTInteractionsPC' prepend(sa)], [part, 10])
